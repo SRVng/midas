6eec25191e80c4fb03d479f8e186ddbd15be497d
Saravut Nakglom <snakglom@gmail.com>
2022-05-08 01:07:02 +0700

    perf: will get deref by complier

--- src/price_manipulation/returns.rs
+++ src/price_manipulation/returns.rs
@@ -5,7 +5,7 @@ pub async fn get_return(prices: &[Decimal]) -> Vec<Decimal> {
     let prices_one_lag = prices[1..].to_vec();
     let prices_len = &prices_one_lag.len();
 
-    let result = remove_with_lag_in_slice(&prices, &prices_one_lag, *prices_len);
+    let result = remove_with_lag_in_slice(prices, &prices_one_lag, *prices_len);
 
     if let Some(x) = result {
         x
