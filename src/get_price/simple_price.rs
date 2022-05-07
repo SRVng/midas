00fd5d834572786e7d1a58623a5b28aa050efcfe
Saravut Nakglom <snakglom@gmail.com>
2022-05-07 23:17:44 +0700

    refactor: use Decimal

--- src/get_price/simple_price.rs
+++ src/get_price/simple_price.rs
@@ -1,23 +1,26 @@
 use reqwest;
+use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Price {
-    usd: f32
+    usd: Decimal,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Avax {
     #[serde(alias = "avalanche-2")]
-    avalanche: Price
+    avalanche: Price,
 }
 
-pub async fn get_current_price(coin_id: &str) -> f32 {
-    let endpoint: String = format!("https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies=usd");
+pub async fn get_current_price(coin_id: &str) -> Decimal {
+    let endpoint: String =
+        format!("https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies=usd");
 
     let client: reqwest::Client = reqwest::Client::new();
 
-    let response: f32 = client.get(endpoint)
+    let response: Decimal = client
+        .get(endpoint)
         .send()
         .await
         .expect("Error at response")
@@ -26,6 +29,6 @@ pub async fn get_current_price(coin_id: &str) -> f32 {
         .expect("Error on parsing to json")
         .avalanche
         .usd;
-    
+
     response
 }
