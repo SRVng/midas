1631a19b79ec813a8b7d86271da57547493b646c
Saravut Nakglom <snakglom@gmail.com>
2022-05-07 23:52:48 +0700

    refactor: :recycle: lifetime can be elided and use '_

--- src/price_manipulation/risks.rs
+++ src/price_manipulation/risks.rs
@@ -4,8 +4,8 @@ use crate::get_return;
 use crate::utils::mean;
 use rust_decimal::{prelude::FromPrimitive, Decimal};
 
-pub async fn get_variance<'a>(mut prices: &'a Box<[Decimal]>) -> Decimal {
-    let mut prices_clone: &'a Box<[Decimal]> = &prices.borrow_mut();
+pub async fn get_variance(mut prices: &'_ Box<[Decimal]>) -> Decimal {
+    let mut prices_clone: &'_ Box<[Decimal]> = &prices.borrow_mut();
     let returns = get_return(prices.clone()).await;
 
     mean(
