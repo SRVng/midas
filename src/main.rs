f1f0e240cc4244a68e201689458dce909b2f1e01
Saravut Nakglom <snakglom@gmail.com>
2022-05-07 23:35:46 +0700

    style: no main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 mod get_price;
+mod price_mutation;
 mod risk_management;
+mod utils;
 
+// GET Price
+use crate::get_price::historical_price::get_historical_chart_data;
 use crate::get_price::simple_price::get_current_price;
 
+// Risk Management
+use crate::risk_management::risk_reward_ratio::calculate_risk_reward;
+
+// Price Mutation
+use crate::price_mutation::returns::get_return;
+use crate::price_mutation::risks::get_variance;
+
 #[tokio::main]
-async fn main() {
-    println!("{:?}", get_current_price("avalanche-2").await);
-}
+async fn main() {}
