55ab995d8bb05cada690349ec3fcb5ce1d6f8fe9
Saravut Nakglom <snakglom@gmail.com>
2022-05-08 01:34:48 +0700

    feat: variance with MathematicalOps

--- src/price_manipulation/risks.rs
+++ src/price_manipulation/risks.rs
@@ -1,15 +1,13 @@
-use std::borrow::BorrowMut;
+use rust_decimal::{prelude::{Decimal, FromPrimitive}, MathematicalOps};
+use crate::price_manipulation::returns::{get_average_return};
 
-use crate::get_return;
-use crate::utils::mean;
-use rust_decimal::{prelude::FromPrimitive, Decimal};
-
-pub async fn get_variance(mut prices: &'_ [Decimal]) -> Decimal {
-    let mut prices_clone: &'_ [Decimal] = prices.borrow_mut();
-    let returns = get_return(prices.clone()).await;
-
-    mean(
-        prices_clone.borrow_mut(),
-        &Decimal::from_usize(prices.len()).unwrap(),
-    )
+pub async fn get_variance(returns: Vec<Decimal>) -> Decimal {
+    let avg_return = get_average_return(returns.clone()).await;
+    let squared_deviation: Decimal = returns
+        .iter()
+        .map(|x| (x - avg_return).powu(2))
+        .sum();
+    let length = Decimal::from_usize(returns.len()).unwrap();
+    
+    squared_deviation / length
 }
