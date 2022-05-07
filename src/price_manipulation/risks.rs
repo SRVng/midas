23500aa87f438c2723e95f07c64af1d668d94812
Saravut Nakglom <snakglom@gmail.com>
2022-05-07 23:56:11 +0700

    refactor: :recycle: borrow_mut already deRef

--- src/price_manipulation/risks.rs
+++ src/price_manipulation/risks.rs
@@ -5,7 +5,7 @@ use crate::utils::mean;
 use rust_decimal::{prelude::FromPrimitive, Decimal};
 
 pub async fn get_variance(mut prices: &'_ [Decimal]) -> Decimal {
-    let mut prices_clone: &'_ [Decimal] = &prices.borrow_mut();
+    let mut prices_clone: &'_ [Decimal] = prices.borrow_mut();
     let returns = get_return(prices.clone()).await;
 
     mean(
