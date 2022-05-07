1e8bc1f947fc263212361b6695d89e1388daedd1
Saravut Nakglom <snakglom@gmail.com>
2022-05-08 01:34:13 +0700

    feat: get average return

--- src/price_manipulation/returns.rs
+++ src/price_manipulation/returns.rs
@@ -1,5 +1,6 @@
 use crate::utils::remove_with_lag_in_slice;
-use rust_decimal::Decimal;
+use rust_decimal::prelude::{FromPrimitive, Decimal};
+use crate::utils::mean;
 
 pub async fn get_return(prices: &[Decimal]) -> Vec<Decimal> {
     let prices_one_lag = prices[1..].to_vec();
@@ -12,4 +13,8 @@ pub async fn get_return(prices: &[Decimal]) -> Vec<Decimal> {
     } else {
         Vec::new()
     }
+}
+
+pub async fn get_average_return(returns: Vec<Decimal>) -> Decimal {
+    mean(&returns, &Decimal::from_usize(returns.len()).unwrap())
 }
